6eec62e92a54c1de39a10d0d9bf42a888ec4bbd6
Анастасия Нестеренко <nesterenko.anastasia1996@gmail.com>
2022-06-04 14:07:46 +0700

    'попытка_11'

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -2,6 +2,7 @@
 Приложение users отвечает за работу с пользователем:
 регистрация, авторизация.
 """
+from api.permissions import IsAdmin
 from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
@@ -11,7 +12,6 @@ from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-from api.permissions import IsAdmin
 
 from .models import User
 from .serializers import (UserAccessTokenSerializer, UserCreationSerializer,
