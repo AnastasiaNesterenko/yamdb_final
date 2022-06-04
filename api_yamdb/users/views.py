574d86cbd85681c383b253f4ec368686ab414476
Анастасия Нестеренко <nesterenko.anastasia1996@gmail.com>
2022-06-04 14:19:41 +0700

    'попытка_12'

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -7,11 +7,11 @@ from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
 
 from .models import User
 from .serializers import (UserAccessTokenSerializer, UserCreationSerializer,
